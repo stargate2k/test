08962756ece28eb32ad9854bc651266b47945eba
Eric <eric.w@kabelbw.de>
2013-09-07 20:42:46 +0200

    First Commit

---
+++ GitTest2013/src/main.java
@@ -0,0 +1,12 @@
+
+public class main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This ist a first Git Test");
+	}
+
+}
