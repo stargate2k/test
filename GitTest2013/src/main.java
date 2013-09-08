aa2fe6d6928a03bff6abe3965662a48f150847cf
stargate2k <3pio@gmx.de>
2013-09-09 00:48:24 +0200

    test

--- GitTest2013/src/main.java
+++ GitTest2013/src/main.java
@@ -5,8 +5,8 @@ public class main {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("This ist a first Git Test33");
-		System.out.println("This ist a first Git Test33");
+		System.out.println("This is a first Git Test33");
+		System.out.println("This is a first Git Test33");
 		//TestTest
 		//Test22
 		//test33
